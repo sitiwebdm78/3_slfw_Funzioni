27bc93295cdcae69fd0893ce61aee8c9eb7567e6
Dario Mauro <sitiwebdm78@gmail.com>
2025-04-28 21:28:17 +0200

    Ho eliminato il file precedente perchè troppo elaborato e ne ho creato uno caome da vostro esempio

--- 3_slfw_funzionimain.js
+++ 3_slfw_funzionimain.js
@@ -5,35 +5,19 @@ Input : 9 → output: 1 cifra
 Input : 99 → output: 2 cifre
 Input: 12000 → output: Numero troppo grande */
 
-let n;
-    do {
-        n = Number(prompt("Inserisci un numero:"));
-        if (n <= 0) {
-            console.log("Per favore, inserisci un numero positivo.");
-        }else if(n === "" || isNaN(n)){
-            console.log("Non puoi inserire caratteri alfabetici ma solo numeri.");
-        }else if(n > 9999){
-            console.log("Numero Troppo Grande.")
-        }
-    } while (isNaN(n) || n <= 0 || n > 9999); // Continua a chiedere finché l'input non è valido.
-
-    function contacifre (numero){
-        let cifra = numero.toString(); 
-            let cifralunga = cifra.length;
-        for(let i = 0; i < cifra.length; i++){
-            if(cifralunga === 1){
-                console.log(`Il Numero inserito è ${numero} ed è composto da 1 cifre`);
-
-            }else if(cifralunga === 2){
-                console.log(`Il Numero inserito è ${numero} ed è composto da 2 cifre`);
-                
-            }else if(cifralunga === 3){
-                console.log(`Il Numero inserito è ${numero} ed è composto da 3 cifre`);
-
-            }else{console.log(`Il Numero inserito è ${numero} ed è composto da 4 cifre`);
-            
-            }
+    function contacifre(numero){
+        if(numero < 10){
+            console.log("Il Numero è composto da 1 cifra");
+        }else if(numero < 100){
+            console.log("Il Numero è composto da 2 cifre");
+        }else if(numero < 1000){
+            console.log("Il Numero è composto da 3 cifre");   
+        }else if(numero < 10000){
+            console.log("Il Numero è composto da 4 cifre");
+        }else{
+            console.log("Numero troppo grande.");
         }
     }
-    
-    contacifre(n);
+
+    contacifre(67659);
+
